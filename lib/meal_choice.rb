bf800225b0771afb9cd2bf66eeb0263f1cd2ff64
Jackson Chen <jacksonchen171@gmail.com>
2020-06-02 21:29:31 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,4 +1,4 @@
 def meal_choice(veg1, veg2, protein = "tofu")
   puts "What a nutritious meal!"
-  puts "A plate of #{veg1} with #{veg1} and #{veg2}."
+  return "A plate of #{veg1} with #{veg1} and #{veg2}."
 end
